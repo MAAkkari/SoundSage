1c7c635e2c4280329a39320a1787fd42f656a4ce
MAAkkari <136325909+MAAkkari@users.noreply.github.com>
2024-01-26 18:34:33 +0100

    correction d'un bug ou le lecteur ne marche plus aprés changement de page

--- public/js/lecteur.js
+++ public/js/lecteur.js
@@ -1,49 +1,60 @@
 
-            $(document).ready(function() {
-                var audio = new Audio('/uploads/files/Radiohead - Reckoner (By Clement Picon).mp3');
-                audio.loop = true;
-                audio.play();
+            // variables globales
+
+            // let trackButtons = null;
             
+            let audio = null;
+            let playPauseBtn = null;
+            let progressBar = null;
+            let progress = null;
+            let volumeSlider = null;
+            let volumeIcon = null;
+            let timeInfo = null;
+
+            // définir/redéfinir les liens des <a> du contenu de #content qui est rechargé en temps réel au changement de page
+            function bindPageLinks(isFirstCall) {
+                console.log("bindPageLinks() - start");
+                let nodeOnWhichBindLinks = null;
+
+                if (isFirstCall) {
+                    nodeOnWhichBindLinks = document;
+                } else {
+                    nodeOnWhichBindLinks = document.getElementById("content");
+                }
+
+                // pour chacun des <a> concernées, définir/redéfinir son comportement au déclenchament de l'évènement
+                // $(document).on('click', 'a:not(.exclude-from-interception)', function(e) {
+                // document.getElementById("#content").querySelectorAll("a:not(.exclude-from-interception)").forEach(aElt => aElt.addEventListener('click', function(e) {
+                nodeOnWhichBindLinks.querySelectorAll("a:not(.exclude-from-interception)").forEach(aElt => aElt.addEventListener('click', async function(e) {
+                    console.log("document.on.click - start");
+                    const href = $(this).attr('href');
                 
-                $(document).on('click', 'a:not(.exclude-from-interception)', function(e) {
-                    var href = $(this).attr('href');
-            
-                  
                     if (doLoadInternally(href)) {
                         e.preventDefault();
-                        loadPage(href);
+                        await loadPage(href);
+                
+                        // if (isFirstCall) {
+                            bindPageLinks(false);
+                        // }
                     }
-                    
-                });
+                    console.log("document.on.click - end");
+                }));
+
+                // trackButtons = document.querySelectorAll('.track-btn');
+                // trackButtons.forEach(button => button.addEventListener('click', changeTrack));
+                console.log("bindPageLinks() - end");
+            }
             
-                function loadPage(url) {
-                    $('#content').load(url + ' #content');
-                }
-            
-                function doLoadInternally(url) {
-                   
-                    return !url.match(/^http[s]?:\/\//);
-                }
-            });
-       
+            async function loadPage(url) {
+                await $('#content').load(url + ' #content');
+            }
         
-            document.addEventListener('DOMContentLoaded', function () {
-            var audio = document.getElementById('audioPlayer');
-            var playPauseBtn = document.getElementById('playPauseBtn');
-            var trackButtons = document.querySelectorAll('.track-btn');
-            var progressBar = document.getElementById('progressBar');
-            var progress = document.getElementById('progress');
-            var volumeSlider = document.getElementById('volumeSlider');
-            var volumeIcon = document.getElementById('volumeIcon');
-            var timeInfo = document.getElementById('timeInfo');
+            function doLoadInternally(url) {
+               
+                return !url.match(/^http[s]?:\/\//);
+            }
 
             
-        
-            playPauseBtn.addEventListener('click', togglePlayPause);
-            trackButtons.forEach(button => button.addEventListener('click', changeTrack));
-            progressBar.addEventListener('click', seekAudio);
-            volumeSlider.addEventListener('input', adjustVolume);
-            audio.addEventListener('timeupdate', updateUI);
         
             function togglePlayPause() {
                 if (audio.paused) {
@@ -55,13 +66,25 @@
                 }
             }
         
-            function changeTrack(event) {
-                var trackSource = event.target.getAttribute('data-track');
-                var albumCover = event.target.getAttribute('data-album-cover');
-                var songName = event.target.getAttribute('data-song-name');
-                var artistName = event.target.getAttribute('data-artist-name');
+            // function changeTrack(event) {
+            //     console.log("changeTrack() - start");
+            //     console.log("changeTrack() - event = ", event);
+            //     const trackSource = event.target.getAttribute('data-track');
+            //     const albumCover = event.target.getAttribute('data-album-cover');
+            //     const songName = event.target.getAttribute('data-song-name');
+            //     const artistName = event.target.getAttribute('data-artist-name');
         
+            function changeTrack(trackButton) {
+                console.log("changeTrack() - start");
+                console.log("changeTrack() - event = ", trackButton);
+                const trackSource = trackButton.getAttribute('data-track');
+                const albumCover = trackButton.getAttribute('data-album-cover');
+                const songName = trackButton.getAttribute('data-song-name');
+                const artistName = trackButton.getAttribute('data-artist-name');
+
+                console.log("changeTrack() - trackSource = ", trackSource);
                 if (trackSource) {
+                    console.log("changeTrack() - audio = ", audio);
                     audio.src = trackSource;
                     audio.load();
                     audio.play();
@@ -72,19 +95,14 @@
                 document.getElementById('albumCover').src = albumCover;
                 document.getElementById('songName').textContent = songName;
                 document.getElementById('artistName').textContent = artistName;
+                console.log("changeTrack() - end");
             }
         
             function seekAudio(e) {
-                var rect = progressBar.getBoundingClientRect();
-                var clickPosition = (e.pageX - rect.left) / progressBar.offsetWidth;
+                const rect = progressBar.getBoundingClientRect();
+                const clickPosition = (e.pageX - rect.left) / progressBar.offsetWidth;
                 audio.currentTime = clickPosition * audio.duration;
             }
-        
-            volumeSlider.addEventListener('input', function () {
-                adjustVolume();
-                updateVolumeSliderBackground(this.value);
-                updateVolumeIcon(volumeSlider.value);
-            });
         
             function adjustVolume() {
                 audio.volume = volumeSlider.value;
@@ -111,7 +129,7 @@
             }
         
             function updateProgressBar() {
-                var percentage = (audio.currentTime / audio.duration) * 100;
+                const percentage = (audio.currentTime / audio.duration) * 100;
                 progress.style.width = percentage + '%';
             }
         
@@ -120,10 +138,47 @@
             }
         
             function formatTime(time) {
-                var minutes = Math.floor(time / 60);
-                var seconds = Math.floor(time % 60);
+                const minutes = Math.floor(time / 60);
+                const seconds = Math.floor(time % 60);
                 return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
             }
+
+
+            
+            // au premier chargement de la page
+            // $(document).ready(function() {
+            document.addEventListener('DOMContentLoaded', function() {
+                // l'import de ce fichier js est possible dans le <head> (et pas forcément juste avant la fin du <body>) car il ne contient que des déclarations de fonctions (et non des appels) (sauf le premier bloc mais qui lui n'est déclenché que lorsque le document / le DOM est prêt)
+                
+                console.log("CALL bindPageLinks() - avant");
+                bindPageLinks(true);
+                console.log("CALL bindPageLinks() - après");
+            
+            audio = document.getElementById('audioPlayer');
+            playPauseBtn = document.getElementById('playPauseBtn');
+            progressBar = document.getElementById('progressBar');
+            progress = document.getElementById('progress');
+            volumeSlider = document.getElementById('volumeSlider');
+            volumeIcon = document.getElementById('volumeIcon');
+            timeInfo = document.getElementById('timeInfo');
+
+            // trackButtons = document.querySelectorAll('.track-btn');
+            
+        
+            playPauseBtn.addEventListener('click', togglePlayPause);
+            // trackButtons.forEach(button => button.addEventListener('click', changeTrack));
+            progressBar.addEventListener('click', seekAudio);
+            volumeSlider.addEventListener('input', adjustVolume);
+            audio.addEventListener('timeupdate', updateUI);
+            
+        
+            volumeSlider.addEventListener('input', function () {
+                adjustVolume();
+                updateVolumeSliderBackground(this.value);
+                updateVolumeIcon(volumeSlider.value);
+            });
+
+
             updateVolumeSliderBackground(volumeSlider.value);
         });
     
